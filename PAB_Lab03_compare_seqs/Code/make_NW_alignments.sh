9f3ce32014c0ff94d661c9903349eb943a7b0ac9
Hans D. Escobar H. <hdescobarh@unal.edu.co>
2024-04-09 02:41:06 -0500

    feat(PAB): ahora lee y genera un csv con los resultados

--- PAB_Lab03_compare_seqs/Code/make_NW_alignments.sh
+++ PAB_Lab03_compare_seqs/Code/make_NW_alignments.sh
@@ -3,13 +3,13 @@
 SEQUENCES=$1
 OUTPUT_FILE=$2
 
-touch $OUTPUT_FILE
+echo "id,alignment" > $OUTPUT_FILE
 
-while read -r line; do
+sed 1d "$SEQUENCES" | while read -r line; do
   # set Internal Field Separator
   IFS=","
   ARRAY=($line)
   ID="${ARRAY[0]}"
   ALIGNMENT=$(perl "./third_party/NW_book_blast_korf.pl" "${ARRAY[1]}" "${ARRAY[2]}")
   printf "%s,%s\n" $ID $(sed -z "s/\n/\\\n/" <<< $ALIGNMENT) >> $OUTPUT_FILE
-done < "$SEQUENCES"
+done
