e0bdc56cd6e13cbd9818df0e14c99a20a3bd11bd
Hans D. Escobar H. <hdescobarh@unal.edu.co>
2024-04-09 01:56:19 -0500

    feat(PAB): agregada base para ejercicio de alineamientos globales

---
+++ PAB_Lab03_compare_seqs/Code/make_NW_alignments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SEQUENCES=$1
+OUTPUT_FILE=$2
+
+touch $OUTPUT_FILE
+
+while read -r line; do
+  # set Internal Field Separator
+  IFS=","
+  ARRAY=($line)
+  ID="${ARRAY[0]}"
+  ALIGNMENT=$(perl "./third_party/NW_book_blast_korf.pl" "${ARRAY[1]}" "${ARRAY[1]}")
+  printf "%s,%s\n" $ID $(sed -z "s/\n/\\\n/" <<< $ALIGNMENT) >> $OUTPUT_FILE
+done < "$SEQUENCES"
