efdc7ef9e2006c7f22a65d3084d6cfd3958bbb98
Hans D. Escobar H. <hdescobarh@unal.edu.co>
2024-04-18 19:55:46 -0500

    refactor(PAB): ahora el ejercicio de alineamiento global no requiere inputs

--- PAB_Lab03_compare_seqs/Code/make_NW_alignments.sh
+++ PAB_Lab03_compare_seqs/Code/make_NW_alignments.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# Author: hdescobarh
 
-SEQUENCES=$1
-OUTPUT_FILE=$2
+cd "$(dirname "$0")" || exit
+mkdir -p "../Results"
 
-echo "id,alignment" > $OUTPUT_FILE
+SEQUENCES="../Data/sequences_for_NW.csv"
+OUTPUT_FILE="../Results/global_alignments.csv"
 
-sed 1d "$SEQUENCES" | while read -r line; do
-  # set Internal Field Separator
-  IFS=","
-  ARRAY=($line)
+echo "id,alignment" >$OUTPUT_FILE
+
+sed 1d "$SEQUENCES" | while IFS="," read -ra ARRAY; do # IFS: Internal Field Separator
   ID="${ARRAY[0]}"
   ALIGNMENT=$(perl "./third_party/NW_book_blast_korf.pl" "${ARRAY[1]}" "${ARRAY[2]}")
-  printf "%s,%s\n" $ID $(sed -z "s/\n/\\\n/" <<< $ALIGNMENT) >> $OUTPUT_FILE
+  FORMATED_ALIGMENT=$(sed -z "s/\n/\\\n/" <<<"$ALIGNMENT")
+  printf "%s,%s\n" "$ID" "$FORMATED_ALIGMENT" >>"$OUTPUT_FILE"
 done
