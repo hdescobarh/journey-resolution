a5ee646c716335e159294a6808cf30414f01a7f5
Hans D. Escobar H. <hdescobarh@unal.edu.co>
2024-04-19 00:03:01 -0500

    feat(PAB): agregado batch script de Slurm. Ejecuta todas las actividades del Lab03

---
+++ PAB_Lab03_compare_seqs/Code/run_batch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author: hdescobarh
+
+#SBATCH --clusters=biocomputo
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=cpu.normal.q
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name=lab03_blast
+#SBATCH --output=lab03_blast-job_%j.out
+#SBATCH --error=lab03_blast-job_%j.err
+
+module purge
+module load envs/anaconda3
+conda activate ~/env
+module load apps/blast+/2.12.0
+
+bash "./make_blast_exercise.sh"
+bash "./make_NW_alignments.sh"
