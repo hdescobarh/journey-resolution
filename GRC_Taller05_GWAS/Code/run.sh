38be627372b921cc8eeb888d90e6bf5fcd6b19e2
Hans D. Escobar H. <hdescobarh@unal.edu.co>
2024-05-31 01:54:12 -0500

    feat(GRC): automatizado pruebas

---
+++ GRC_Taller05_GWAS/Code/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+OUTPUT_DIR="../Results"
+HAPMAP_FILE="../Example_Data/GBS_150G.txt"
+PHENOTYPE_FILE="../Example_Data/pheno.txt"
+COMPONENTS=("3" "4" "6")
+
+# Use this script location as reference directory
+cd "$(dirname "$0")" || exit
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR" || exit
+
+for CURRENT in "${COMPONENTS[@]}"; do
+  OUT="ncomponents_$CURRENT"
+  rm -rf "$OUT"
+  mkdir "$OUT"
+  cd "$OUT" || exit
+  Rscript --vanilla "./gwas_mixed_linear.R" "$PHENOTYPE_FILE" "$HAPMAP_FILE" "$CURRENT" 2>&1 | tee "_script_report.txt"
+  cd ..
+done
