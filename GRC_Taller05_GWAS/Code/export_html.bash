d6cbb340350d0e7cea072a3a6831c8d419d97cb3
Hans D. Escobar H. <hdescobarh@unal.edu.co>
2024-06-25 13:54:03 -0500

    refactor(GRC): ahora guarda el HTML en una carpeta fuera de Results

--- GRC_Taller05_GWAS/Code/export_html.bash
+++ GRC_Taller05_GWAS/Code/export_html.bash
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")" || exit
 notebook="solucion_taller05.ipynb"
-output_dir="../Results"
+output_dir="../HTML_index"
 jupyter nbconvert $notebook --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_input_tags remove_input --TagRemovePreprocessor.remove_cell_tags remove_cell --to html --output-dir=$output_dir
